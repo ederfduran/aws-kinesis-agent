e552ef9e2bc976a33169c4175db4ce50fbfddf05
Eder Duran <ederduran@Eders-MacBook-Pro.local>
2021-07-22 20:52:58 -0500

    add readme

--- clean_up.sh
+++ clean_up.sh
@@ -3,6 +3,9 @@ S3_BUCKET="kinesis-agent-demo-eder-2021"
 
 aws s3 rm s3://$S3_BUCKET/ --recursive 2>&1
 aws s3api delete-bucket --bucket $S3_BUCKET 2>&1
-aws cloudformation stack-delete-complete --stack-name aws-kinesis-agent-resources
+aws cloudformation delete-stack --stack-name aws-kinesis-agent-resources
+echo "deleting stack ....."
+aws cloudformation wait stack-delete-complete --stack-name aws-kinesis-agent-resources
+echo "Stack deleted"
 
 aws s3 ls
